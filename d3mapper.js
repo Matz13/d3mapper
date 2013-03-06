908d4d95d7d7740c40bff80505226ca85af30a56
Mathieu Chesnel <maz13@gmail.com>
2013-03-05 23:31:13 -0500

    Datasets transition is working
    with animated transitions
    hover is fixed
    unavailable data is striped

--- d3mapper.js
+++ d3mapper.js
@@ -2,12 +2,12 @@
 
 //defines the configuration of the map
 
-	var primary_duration_config = {mapTitle : "Duration of Mandatory primary school (2011)", fileToLoad : "se_prm_durs.csv", datatype : "quantitative", colorscale : colorbrewer.RdYlGn[10], legendScaleRange : [0,9], mapScaleDomain : [0,1,2,3,4,5,6,7,8,9]};
+	var primary_duration_config = {mapTitle : "Duration of Mandatory primary school (2011)", fileToLoad : "se_prm_durs.csv", datatype : "quantitative", colorscale : colorbrewer.RdYlGn[7], legendScaleRange : [3,9], mapScaleDomain : [3,4,5,6,7,8,9]};
 	var population_2010_config = {mapTitle : "Population (2010)", fileToLoad : "population2010.csv", datatype : "quantitative", colorscale : colorbrewer.Purples[6], legendScaleRange : [0,200000000], mapScaleDomain : [5000000,10000000,25000000,50000000,100000000]}
 	var outofschool_prm_2011_config = {mapTitle : "Measures of children out of school (2011)", fileToLoad : "outofschoolprimary2011.csv", datatype : "qualitative", colorscale : colorbrewer.Reds[6], legendScaleRange : [0,.63], mapScaleDomain : [.01,.05,.1,.2,.30]};
 
 if(!config){
-	var config = population_2010_config;
+	var config = outofschool_prm_2011_config;
 }
 
 
@@ -18,21 +18,21 @@ if(!config){
 // add links to load the different datasets
 d3.select("#controls")
 	.append("a")
-	.attr("href","javascript:void()")
+	.attr("href","javascript:void(0)")
 	.attr("onclick","draw(outofschool_prm_2011_config)")
-	.text("OOSC ");
+	.text("Out of School Children 2011");
 
 d3.select("#controls")
 	.append("a")
-	.attr("href","javascript:void()")
+	.attr("href","javascript:void(0)")
 	.attr("onclick","draw(population_2010_config)")
-	.text("Pop ");
+	.text("Population 2010");
 
 d3.select("#controls")
 	.append("a")
-	.attr("href","javascript:void()")
+	.attr("href","javascript:void(0)")
 	.attr("onclick","draw(primary_duration_config)")
-	.text("Prm duration ");
+	.text("Primary school duration 2011");
 
 var w = $('#d3mapper').width();        //width of the div
 var h = $('#d3mapper').height();        //height of the div
@@ -47,7 +47,7 @@ else{mapSize.w = d3.round(h*r); mapSize.h = h;}
 
 var projection = d3.geo.naturalEarth()
 	.scale(mapSize.w/5.5)
-	.translate([mapSize.w/2, (mapSize.h/2)*1.05]);
+	.translate([mapSize.w/2, (mapSize.h/2)*1.08]);
 
 var path = d3.geo.path()
 	.projection(projection);
@@ -93,19 +93,45 @@ var legend2 = svg.append("g")
 	.attr("id", "legend2")
 	.attr("transform","translate("+d3.round(mapSize.w*.025)+","+d3.round(mapSize.h-25)+")");
 
+// create the striped pattern for unavailable data
+svg.append("defs")
+	.append("pattern")
+		.attr("id","striped")
+		.attr("width", 5)
+		.attr("height", 5)
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("patternUnits","userSpaceOnUse")
+		.append("rect")
+		.attr("width", 5)
+		.attr("height", 5)
+		.attr("fill","#ccc");
+		
+	d3.select("#striped")
+		.append("path")
+			.attr("d","M0,0 L5,5")
+			.attr("stroke","black")
+			.attr("stroke-width","1");
+
+var cVal = {};
+var dVal = {};
+var valScale = d3.scale.threshold();
+
 //----------------------- Loading values from the CSV
 function draw(config){
 
 d3.csv(config.fileToLoad, function(error,rawPop){
 	d3.json("world_countries.json", function(json) {
 		
-		var cVal = {};
+		cVal = {};
 		rawPop.forEach(function(d, i){
-			cVal[d.countryCode] = +d.val;
+			if(d.val !== undefined){
+				cVal[d.countryCode] = d.val;
+			}
 		});
-		var dVal = rawPop;
+		dVal = rawPop;
 	
-		var valScale = d3.scale.threshold()
+		valScale
 			.domain(config.mapScaleDomain)
 			.range(config.colorscale);
 			
@@ -128,6 +154,9 @@ d3.csv(config.fileToLoad, function(error,rawPop){
 			var formatMapVal = d3.format("%");
 		}
 		
+		legend2.selectAll("*").remove();
+//		countries.selectAll("title").remove();
+		
 		legend2.selectAll("rect")
 			.data(valScale.range().map(function(d, i){
 				return{
@@ -138,35 +167,62 @@ d3.csv(config.fileToLoad, function(error,rawPop){
 			}))
 			.enter().append("rect")
 				.attr("height",8)
-				.attr("x",function(d){return d.x0;})
-				.attr("width",function(d){return d.x1 - d.x0;})
 				.style("fill",function(d){return d.z;})
-				.transition();
+				.transition()
+					.duration(1000)
+					.attr("width",function(d){return d.x1 - d.x0;})
+					.attr("x",function(d){return d.x0;})
+;
+
 		
-		legend2.call(xAxis).append("text")
+		legend2.append("g")
+			.transition()
+			.duration(1000)
+			.call(xAxis);
+			
+		legend2	
+			.append("text")
 			.attr("class","caption")
 			.attr("y", -6)
 			.text(config.mapTitle+". (Hover a country to get more information)");
 // end of legend
+		var oldColor;
 				
 		countries.selectAll("path")
 			.data(json.features)
 			.enter().append("path")
-				.attr("svg:name", function (d) {return d.properties.name;})
+				.attr("name", function (d) {return d.properties.name;})
 				.attr("id", function (d) {return d.id;})
 				.attr("d", path)
 				.on('mouseover',function(d){
-					this.style.fill = d3.rgb(valScale(cVal[d.id])).brighter()})
-				.on('mouseout', function(d){this.style.fill = valScale(cVal[d.id])})
-				.append("title").text(function (d) {return d.properties.name+"\nValue: "+formatMapVal(cVal[d.id]);});
+					oldColor = this.style.fill;
+					if(cVal[d.id] !== "" && cVal[d.id] !== undefined){ 
+						this.style.fill = d3.rgb(this.style.fill).brighter()
+					}
+				})
+					
+				.on('mouseout', function(d){
+					if(cVal[d.id] !== "" && cVal[d.id] !== undefined){ 
+						this.style.fill = oldColor
+					}	
+				})	
+				.append("title");
 	
 		countries.selectAll("path")
 				.transition()
 				.duration(1000)
-				.style("fill", function(d){					
-					if(cVal[d.id] === undefined){return "#666666";}
+				.style("fill", function(d){
+					if(cVal[d.id] == undefined || cVal[d.id] == ""){return "url(#striped)";}
 					else{ return valScale(cVal[d.id]);}
-				});
+				})
+				.select("title").text(function (d) {
+					if(cVal[d.id] == undefined || cVal[d.id] == ""){
+						return d.properties.name+"\nValue not available ";
+					}else{
+						return d.properties.name+"\nValue: "+formatMapVal(cVal[d.id]);
+					}
+				})
+				;
 	});
 });
 }
